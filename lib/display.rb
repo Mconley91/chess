6cb42463b1aac47e24c3c915dae8e2265df3b40e
mconley91 <matthew.e.conley@gmail.com>
2025-06-20 19:22:45 -0400

    Remove methods for algebraic notation. Edit  #display_board to implement algebraic notation  automatically.

--- lib/display.rb
+++ lib/display.rb
@@ -3,15 +3,13 @@
 module Display
   
   def display_board
-    @game_board.squares.each{|row| puts "#{row}"}
-  end
-
-  def apply_algebraic_notation
-    @game_board.algebraic_notation
+    @game_board.squares.each_with_index do |row, index| 
+      puts "#{8 - index} |#{row[0]}|#{row[1]}|#{row[2]}|#{row[3]}|#{row[4]}|#{row[5]}|#{row[6]}|#{row[7]}|"
+    end
+    puts "   A B C D E F G H"
   end
 
   def display_game
-    apply_algebraic_notation
     display_board
   end
 end
