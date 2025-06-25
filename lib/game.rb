d6d454f844a4ba12986c984a479952e53fd2cce9
mconley91 <matthew.e.conley@gmail.com>
2025-06-25 16:18:51 -0400

    Add input.rb, move game class methods handling input to the input module

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,7 @@
 require './lib/board.rb'
 require './lib/rules.rb'
 require './lib/display.rb'
+require './lib/input.rb'
 
 class Game 
   attr_accessor :game_board, :player_turn, :round, :turn
@@ -15,6 +16,7 @@ class Game
   end
 
   include Display
+  include Input
 
   def set_game
     @game_board.set_piece_positions
@@ -40,40 +42,6 @@ class Game
       @selected_piece = nil
       @selected_square = nil
     end
-  end
-
-  def get_input
-    input = gets.chomp.downcase.split('')
-    return convert_to_yx(input) if play_validator(input)
-  end
-
-  def convert_to_yx(input)
-    yx = [nil,nil]
-    yx[0] = 8 - input[1].to_i
-    case input[0]
-      when 'a' 
-        yx[1] = 0
-      when 'b' 
-        yx[1] = 1
-      when 'c' 
-        yx[1] = 2
-      when 'd' 
-        yx[1] = 3
-      when 'e' 
-        yx[1] = 4
-      when 'f' 
-        yx[1] = 5
-      when 'g' 
-        yx[1] = 6
-      when 'h' 
-        yx[1] = 7
-    end
-    yx
-  end
-
-  def play_validator(play)
-    return true if  %w"a b c d e f g h".any?(play[0]) && play[1].to_i.between?(1,8) && play.length == 2
-    false
   end
 
   def next_turn
---
+++ lib/input.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+module Input
+  def get_input
+    input = gets.chomp.downcase.split('')
+    return convert_to_yx(input) if play_validator(input)
+  end
+
+  def convert_to_yx(input)
+    yx = [nil,nil]
+    yx[0] = 8 - input[1].to_i
+    case input[0]
+      when 'a' 
+        yx[1] = 0
+      when 'b' 
+        yx[1] = 1
+      when 'c' 
+        yx[1] = 2
+      when 'd' 
+        yx[1] = 3
+      when 'e' 
+        yx[1] = 4
+      when 'f' 
+        yx[1] = 5
+      when 'g' 
+        yx[1] = 6
+      when 'h' 
+        yx[1] = 7
+    end
+    yx
+  end
+
+  def play_validator(play)
+    return true if  %w"a b c d e f g h".any?(play[0]) && play[1].to_i.between?(1,8) && play.length == 2
+    false
+  end
+end
+
