6543689ad954fa2e6159bd3eec4a1d07385f26b1
mconley91 <matthew.e.conley@gmail.com>
2025-06-26 19:58:12 -0400

    Delete #display_board, add contents directly to  #display_game

--- lib/display.rb
+++ lib/display.rb
@@ -1,18 +1,14 @@
 # frozen_string_literal: true
 
 module Display
-  
-  def display_board
+
+  def display_game
     @game_board.squares.each_with_index do |row, index| 
       puts "#{8 - index} |#{row[0]}|#{row[1]}|#{row[2]}|#{row[3]}|#{row[4]}|#{row[5]}|#{row[6]}|#{row[7]}|"
     end
     puts "   A B C D E F G H"
     puts "ROUND: #{self.round}, #{self.player_turn}'s move"
     puts @selected_piece ? "Selected: #{@selected_piece.icon}" : "select a piece to play"
-  end
-
-  def display_game
-    display_board
   end
 
 
