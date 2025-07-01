d06f557e053a0d82e82b0869f96048db02512ab9
mconley91 <matthew.e.conley@gmail.com>
2025-07-01 11:49:33 -0400

    Add rook basic horizontal & vertical move rules

--- lib/pieces/rook.rb
+++ lib/pieces/rook.rb
@@ -11,6 +11,13 @@ class Rook < Piece
   end
 
   def legal_move?(target, selected_color, clears, solids)
-    
+    #returns true for all horizontal and vertical moves
+    #can not jump over other pieces
+    #may land on squares occupied by enemy pieces
+    if target
+      return true if target[0].between?(0,7) && target[1] == self.yx[1] #vertical move
+      return true if target[0] == self.yx[0] && target[1].between?(0,7) #horizontal move
+    end
   end
+
 end
