36484a5baaf09f378a7e0258b1d8c8d7d7e3b1cb
mconley91 <matthew.e.conley@gmail.com>
2025-07-01 18:08:07 -0400

    Refactor #legal_move? for Rook objects

--- lib/pieces/rook.rb
+++ lib/pieces/rook.rb
@@ -12,6 +12,8 @@ class Rook < Piece
 
   def legal_move?(target, selected_color, clears, solids)
     if target
+      all_pieces = clears + solids
+
       # ranges are ordered low to high
       y_range = self.yx[0] > target[0] ? (target[0]..self.yx[0]).to_a : (self.yx[0]..target[0]).to_a
       x_range = self.yx[1] > target[1] ? (target[1]..self.yx[1]).to_a : (self.yx[1]..target[1]).to_a
@@ -21,10 +23,10 @@ class Rook < Piece
       x_squares = x_range.map{|x_cord| [self.yx[0], x_cord]}
 
       # prevents 'jumping' over pieces between self & target
-      pieces_on_y = y_squares.find{|coords| solids.any?{|piece| piece.yx == coords && piece != self && piece.yx != target && piece.in_play} ||
-       clears.any?{|piece| piece.yx == coords && piece != self && piece.yx != target && piece.in_play}}
-      pieces_on_x = x_squares.find{|coords| solids.any?{|piece| piece.yx == coords && piece != self && piece.yx != target && piece.in_play} ||
-       clears.any?{|piece| piece.yx == coords && piece != self && piece.yx != target && piece.in_play}}
+      pieces_on_y = y_squares.find{|coords| all_pieces.any?{|piece| piece.yx == coords && piece != self &&
+       piece.yx != target && piece.in_play}}
+      pieces_on_x = x_squares.find{|coords| all_pieces.any?{|piece| piece.yx == coords && piece != self && 
+       piece.yx != target && piece.in_play}}
 
       return true if target[0].between?(0,7) && target[1] == self.yx[1] && self.yx[0] && !pieces_on_y # vertical move
       return true if target[0] == self.yx[0] && target[1].between?(0,7) && !pieces_on_x # horizontal move
