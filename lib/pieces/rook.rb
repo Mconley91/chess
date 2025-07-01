d9a964620f442ad733439f2a8f7d398d385ed067
mconley91 <matthew.e.conley@gmail.com>
2025-07-01 17:58:18 -0400

    Add 'no jumping' logic to rook pieces

--- lib/pieces/rook.rb
+++ lib/pieces/rook.rb
@@ -11,12 +11,23 @@ class Rook < Piece
   end
 
   def legal_move?(target, selected_color, clears, solids)
-    #returns true for all horizontal and vertical moves
-    #can not jump over other pieces
-    #may land on squares occupied by enemy pieces
     if target
-      return true if target[0].between?(0,7) && target[1] == self.yx[1] #vertical move
-      return true if target[0] == self.yx[0] && target[1].between?(0,7) #horizontal move
+      # ranges are ordered low to high
+      y_range = self.yx[0] > target[0] ? (target[0]..self.yx[0]).to_a : (self.yx[0]..target[0]).to_a
+      x_range = self.yx[1] > target[1] ? (target[1]..self.yx[1]).to_a : (self.yx[1]..target[1]).to_a
+
+      # makes usable coordinates from ranges
+      y_squares = y_range.map{|y_cord| [y_cord, self.yx[1]]}
+      x_squares = x_range.map{|x_cord| [self.yx[0], x_cord]}
+
+      # prevents 'jumping' over pieces between self & target
+      pieces_on_y = y_squares.find{|coords| solids.any?{|piece| piece.yx == coords && piece != self && piece.yx != target && piece.in_play} ||
+       clears.any?{|piece| piece.yx == coords && piece != self && piece.yx != target && piece.in_play}}
+      pieces_on_x = x_squares.find{|coords| solids.any?{|piece| piece.yx == coords && piece != self && piece.yx != target && piece.in_play} ||
+       clears.any?{|piece| piece.yx == coords && piece != self && piece.yx != target && piece.in_play}}
+
+      return true if target[0].between?(0,7) && target[1] == self.yx[1] && self.yx[0] && !pieces_on_y # vertical move
+      return true if target[0] == self.yx[0] && target[1].between?(0,7) && !pieces_on_x # horizontal move
     end
   end
 
