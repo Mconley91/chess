f7a4d29e6c4f0ca9e8df21a7b8d51e4c40b78a36
mconley91 <matthew.e.conley@gmail.com>
2025-07-02 19:58:55 -0400

    Add #plot_path & #piece_in_path? methods to Piece superclass. Bishop class implements methods.

--- lib/piece.rb
+++ lib/piece.rb
@@ -6,4 +6,26 @@ class Piece
     @in_play = true
   end
 
+  def plot_path(piece, target)
+    path = [piece]
+    until path.last == target
+      p path
+      return nil if path.length > 8 # prevents game break when path unobtainable 
+      y_coord = target[0] == piece[0] ? 0 : target[0] > piece[0] ? 1 : -1
+      x_coord = target[1] == piece[1] ? 0 : target[1] > piece[1] ? 1 : -1
+      path << [path.last[0] + y_coord, path.last[1] + x_coord]
+    end
+    path
+  end
+
+  def piece_in_path?(path, all_pieces)
+    if path
+      return true if path.find{|coords| all_pieces.any?{|piece| piece.yx == coords && piece.yx != path.first && piece.yx != path.last && piece.in_play}}
+      return false
+    elsif path == nil
+      return true
+    end
+    false
+  end
+
 end
--- lib/pieces/bishop.rb
+++ lib/pieces/bishop.rb
@@ -13,18 +13,8 @@ class Bishop < Piece
   def legal_move?(target, selected_color, clears, solids)
     if target 
       all_pieces = clears + solids
-      squares = [self.yx]
-
-      until squares.last == target
-        y_coord = target[0] > self.yx[0] ? 1 : -1
-        x_coord = target[1] > self.yx[1] ? 1 : -1
-        squares << [squares.last[0] + y_coord, squares.last[1] + x_coord]
-      end
-
-      # prevents 'jumping' over pieces between self & target
-      pieces_between = squares.find{|coords| all_pieces.any?{|piece| piece.yx == coords && piece != self && piece.yx != target && piece.in_play}}
-
-      return true if target[1] != self.yx[1] && target[0] != self.yx[0] && !pieces_between
+      return true if target[0] != self.yx[0] && target[1] != self.yx[1] && 
+       self.piece_in_path?(self.plot_path(self.yx, target), all_pieces) == false
     end
   end
 
