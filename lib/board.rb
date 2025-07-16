28264b25bdcb1083f1eb0bbd4dd91031249cc314
mconley91 <matthew.e.conley@gmail.com>
2025-07-16 19:28:59 -0400

    Finish check logic.
    TODO: Checkmate, castling, pawn promotion,
    stalemate, threefold repetition draw, &
    insufficient material draw

--- lib/board.rb
+++ lib/board.rb
@@ -19,7 +19,7 @@ class Board
   def make_pieces(color)
     if color == 'clear'
       arr = []
-      # @squares[6].each_with_index{|square,index| arr << Pawn.new(color,[6,index])}
+      @squares[6].each_with_index{|square,index| arr << Pawn.new(color,[6,index])}
       arr << Rook.new(color,[7,0])
       arr << Rook.new(color,[7,7])
       arr << Knight.new(color,[7,1])
@@ -31,14 +31,14 @@ class Board
       arr
     else # solid pieces
       arr = []
-      # @squares[1].each_with_index{|square,index| arr << Pawn.new(color,[1,index])}
-      # arr << Rook.new(color,[0,0])
-      # arr << Rook.new(color,[0,7])
-      # arr << Knight.new(color,[0,1])
-      # arr << Knight.new(color,[0,6])
-      # arr << Bishop.new(color,[0,2])
-      # arr << Bishop.new(color,[0,5])
-      # arr << Queen.new(color,[0,3])
+      @squares[1].each_with_index{|square,index| arr << Pawn.new(color,[1,index])}
+      arr << Rook.new(color,[0,0])
+      arr << Rook.new(color,[0,7])
+      arr << Knight.new(color,[0,1])
+      arr << Knight.new(color,[0,6])
+      arr << Bishop.new(color,[0,2])
+      arr << Bishop.new(color,[0,5])
+      arr << Queen.new(color,[0,3])
       arr << King.new(color,[0,4])
       arr
     end   
